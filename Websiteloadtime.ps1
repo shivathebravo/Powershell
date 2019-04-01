4fbf1f331e20456436793212cb9005fad595ea92
Shiva Sharma <shivathebravo@gmail.com>
2019-04-01 16:15:46 -0400

    added script

--- Websiteloadtime.ps1
+++ Websiteloadtime.ps1
@@ -1,9 +1,9 @@
-$url = "http:shivaprogramming.com"
+$url = 'http://shivaprogramming.com'
 
-$timetaken = Measured-Command -Expression {
-
-    $site = Invoke-WebRequest -Uri $url
+$timetaken = Measure-Command -Expression {
+  $site = Invoke-WebRequest -Uri $url
 }
+
 $seconds = [Math]::Round($timetaken.TotalSeconds, 4)
 
 "The page took $seconds seconds to load"
